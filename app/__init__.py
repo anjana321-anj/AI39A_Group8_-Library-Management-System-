1ebb1356f7bd6fcb0d3ff06a8d314a87217f3bad
Animshr <animshr123@gmail.com>
2026-05-26 11:59:10 +0545

    Stage 3: Add database and core app config

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
-from flask import Flask
-from app.routes.auth import AuthRoutes
 import os
+
+from flask import Flask
+
+from app.database import initialize_mysql_database
+from app.routes.auth import AuthRoutes
 
 def create_app():
     # Configure Flask to serve both static and image folders
-    app = Flask(__name__, 
+    app = Flask(__name__,
                 static_url_path='/static',
                 static_folder=os.path.join(os.path.dirname(__file__), 'static'))
+    app.config.from_object("config")
+    app.secret_key = app.config["SECRET_KEY"]
+    initialize_mysql_database()
     
     # Add image folder as an additional static folder
     @app.route('/image/<filename>')
