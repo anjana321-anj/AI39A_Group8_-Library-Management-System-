723bb7ed01a95fa820486787b48cd73add21007f
Aendawi Baral <aendawibaral@gmail.com>
2026-06-20 21:28:42 +0545

    feat: integrated and tracked config.py from Anjana branch

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import pymysql
 SECRET_KEY = os.getenv("SECRET_KEY", "random-secret-key")
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "thulolado")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "Anjana@2064")
 MYSQL_DB = os.getenv("MYSQL_DB", os.getenv("MYSQL_DATABASE", "class_db"))
 MYSQL_DATABASE = MYSQL_DB
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
